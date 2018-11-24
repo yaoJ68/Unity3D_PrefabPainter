d3f43ec85a02e3ca9224243a86b5076f02c3705f
Yao Jiang <yaj889@mail.usask.ca>
2018-11-24 17:20:51 -0600

    - can draw, need adjust drawing position

--- PrefabPainter2D/Assets/Scripts/Window.cs
+++ PrefabPainter2D/Assets/Scripts/Window.cs
@@ -4,13 +4,9 @@ using UnityEngine;
 using UnityEditor;
 
 public class Window : EditorWindow {
-    private Object firstPrefab;
+    public GameObject firstPrefab;
     private bool isPainting;
 
-    private void Awake()
-    {
-        firstPrefab = AssetDatabase.LoadAssetAtPath("Assets/Resources/clover.prefab", typeof(Object));
-    }
     // draw the window
     [MenuItem("Window/PrefabPainter")]
     public static void ShowWindow(){
@@ -22,6 +18,7 @@ public class Window : EditorWindow {
         Event e = Event.current;
         GUILayout.Space(20);
 
+        firstPrefab = (GameObject)EditorGUILayout.ObjectField(firstPrefab, typeof(GameObject), true);
         // paint buttons
         GUILayout.BeginHorizontal();
 
@@ -31,14 +28,36 @@ public class Window : EditorWindow {
         GUILayout.EndHorizontal();
     }
 
-    //private void Update()
-    //{
-    //    if (Input.GetMouseButtonDown(0)){
-    //        Debug.Log("click");
-    //    }
-    //}
+    private void OnEnable()
+    {
+        SceneView.onSceneGUIDelegate -= CustomeUpdate;
+        SceneView.onSceneGUIDelegate += CustomeUpdate;
+    }
 
-    public bool IsPainting(){
-        return isPainting;
+    void CustomeUpdate(UnityEditor.SceneView sv){
+        Event e = Event.current;
+
+        if (e.type == EventType.MouseDown && e.button == 0 && isPainting){
+            Vector3 mousePosition = e.mousePosition;
+            mousePosition.y = sv.camera.pixelHeight - mousePosition.y;
+            mousePosition.y = -mousePosition.y;
+            mousePosition = sv.camera.ScreenToWorldPoint(e.mousePosition);
+            mousePosition.z = 0;
+
+            GameObject placedObject = (GameObject)PrefabUtility.InstantiatePrefab(firstPrefab);
+            placedObject.transform.position = mousePosition;
+            placedObject.transform.localScale = new Vector3(1, 1, 1);
+
+            //RaycastHit hit;
+
+            //Ray ray = Camera.current.ScreenPointToRay(new Vector3(e.mousePosition.x, Camera.current.pixelHeight - e.mousePosition.y, 0));
+            //if (Physics.Raycast(ray, out hit, Mathf.Infinity, ~LayerMask.NameToLayer("Default"))){
+            //    GameObject placedObject = (GameObject)PrefabUtility.InstantiatePrefab(PrefabUtility.GetCorrespondingObjectFromSource(firstPrefab));
+            //    Debug.Log("click");
+            //    placedObject.transform.position = hit.point;
+            //    placedObject.transform.localScale = new Vector3(1, 1, 1);
+            //}
+
+        }
     }
 }
